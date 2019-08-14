44a93d0c47f0bee014884052110ddf88a403d522
XieYaqi <987159036@qq.com>
2019-08-14 22:42:38 +0800

    远程调用的三种方式

--- order/server/src/main/java/com/xxxx/order/controller/ClientController.java
+++ order/server/src/main/java/com/xxxx/order/controller/ClientController.java
@@ -1,6 +1,9 @@
 package com.xxxx.order.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -9,11 +12,32 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class ClientController {
 
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/getProductMsg")
     public String getProductMsg() {
-        // 1.第一种方式
+        // 1.第一种方式(直接使用restTemplate，url写死)
+//        RestTemplate restTemplate = new RestTemplate();
+//        String response = restTemplate.getForObject("http://localhost:8081/msg", String.class);
+
+
+
+        // 2.第二种方式(利用loadBalancerClient通过应用名获取url，然后再使用restTemplate)
         RestTemplate restTemplate = new RestTemplate();
-        String response = restTemplate.getForObject("http://localhost:8081/msg", String.class);
+        ServiceInstance serviceInstance = loadBalancerClient.choose("PRODUCT");
+        String host = serviceInstance.getHost();
+        int port = serviceInstance.getPort();
+
+        String url = String.format("http://%s:%s", host, port) + "/msg";
+        String response = restTemplate.getForObject(url, String.class);
+
+
+        // 3.第三种方式(利用@LoadBalanced，可在restTemplate里使用应用名字)
+        String response = restTemplate.getForObject("http://PRODUCT/msg", String.class);
 
         log.info("response: {}", response);
         return response;
