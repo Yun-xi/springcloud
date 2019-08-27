275aa697b6d221c5d7ca6ebce63ceb13806299d3
XieYaqi <987159036@qq.com>
2019-08-27 22:29:43 +0800

    redis缓存数量

--- order/server/src/main/java/com/xxxx/order/message/ProductInfoReceiver.java
+++ order/server/src/main/java/com/xxxx/order/message/ProductInfoReceiver.java
@@ -5,6 +5,8 @@ import com.xxxx.product.common.ProductInfoOutput;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,6 +18,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class ProductInfoReceiver {
 
+    private static final String PRODUCT_STOCK_TEMPLATE = "product_stock_%s";
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+
     @RabbitListener(queuesToDeclare = @Queue("productInfo"))
     public void process(String message) {
         // message => ProductInfoOutput
@@ -23,6 +30,6 @@ public class ProductInfoReceiver {
         log.info("从队列 {} 接收到消息: {}", "productInfo", productInfoOutput);
 
         // 存到redis
-
+        stringRedisTemplate.opsForValue().set(String.format(PRODUCT_STOCK_TEMPLATE, productInfoOutput), String.valueOf(productInfoOutput.getProductStock()));
     }
 }
