73342f92fa5b83ba874254686320158f0d87b8f4
XieYaqi <987159036@qq.com>
2019-09-01 22:21:12 +0800

    zuul gateway

---
+++ api-gateway/src/main/java/com/xxxx/apigateway/exception/RateLimitException.java
@@ -0,0 +1,4 @@
+package com.xxxx.apigateway.exception;
+
+public class RateLimitException extends RuntimeException{
+}
---
+++ api-gateway/src/main/java/com/xxxx/apigateway/filter/RateLimiterFilter.java
@@ -0,0 +1,48 @@
+package com.xxxx.apigateway.filter;
+
+import com.google.common.util.concurrent.RateLimiter;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import com.xxxx.apigateway.exception.RateLimitException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.SERVLET_DETECTION_FILTER_ORDER;
+
+/**
+ * 限流
+ */
+@Component
+public class RateLimiterFilter extends ZuulFilter {
+
+    private static final RateLimiter RATE_LIMITER = RateLimiter.create(100);
+
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    /**
+     * 因为限流的优先级最高，所以拿到最小的SERVLET_DETECTION_FILTER_ORDER值为-3
+     * 在他的基础上再减去1
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return SERVLET_DETECTION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        if (!RATE_LIMITER.tryAcquire()) {
+            throw new RateLimitException();
+        }
+        return null;
+    }
+}
