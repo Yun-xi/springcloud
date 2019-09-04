bba9f8679c5e5a9b79698ebb9b8c42ef9e555aff
xieyaqi <987159036@qq.com>
2019-09-04 17:55:18 +0800

    feign 使用hystrix

--- product/client/src/main/java/com/xxxx/product/client/ProductClient.java
+++ product/client/src/main/java/com/xxxx/product/client/ProductClient.java
@@ -3,6 +3,7 @@ package com.xxxx.product.client;
 import com.xxxx.product.common.DecreaseStockInput;
 import com.xxxx.product.common.ProductInfoOutput;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -12,7 +13,7 @@ import java.util.List;
  * Created by xieyaqi
  * 2017-12-10 21:04
  */
-@FeignClient(name = "product")
+@FeignClient(name = "product", fallback = ProductClient.ProductClientFallback.class)
 public interface ProductClient {
 
     @PostMapping("/product/listForOrder")
@@ -20,4 +21,18 @@ public interface ProductClient {
 
     @PostMapping("/product/decreaseStock")
     void decreaseStock(@RequestBody List<DecreaseStockInput> decreaseStockInputList);
+
+    @Component
+    static class ProductClientFallback implements ProductClient {
+
+        @Override
+        public List<ProductInfoOutput> listForOrder(List<String> productIdList) {
+            return null;
+        }
+
+        @Override
+        public void decreaseStock(List<DecreaseStockInput> decreaseStockInputList) {
+
+        }
+    }
 }
